20c71bd001919eccf146dae81788652c37a72b2f
Madhunarayana26 <madhunarayana2603@gmail.com>
2023-05-20 13:46:01 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def srujana(request):
+    return HttpResponse('srujana tinnava raaa')
+def bangaram(request):
+    return HttpResponse('chi po da arey.........')
